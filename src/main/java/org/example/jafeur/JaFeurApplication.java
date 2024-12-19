d0e061c11db46c1ae909515e873652a8e0ebc3a4
noa.despaux <noa.despaux@etu.umontpellier.fr>
2024-12-19 14:25:57 +0100

    Initial commit

---
+++ src/main/java/org/example/jafeur/JaFeurApplication.java
@@ -0,0 +1,13 @@
+package org.example.jafeur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaFeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaFeurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/jafeur/JaFeurApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jafeur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaFeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
