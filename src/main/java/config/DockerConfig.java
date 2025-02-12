713a864107f32cab17f45f1ef51ea72d3610122c
auriane.pusel <auriane.pusel@etu.umontpellier.fr>
2025-02-12 15:53:54 +0100

    commit de test pck jsplus ou on en est

---
+++ src/main/java/config/DockerConfig.java
@@ -0,0 +1,29 @@
+package config;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+
+@Configuration
+public class DockerConfig {
+
+    @Bean
+    DockerClient dockerClient() {
+        var config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .build();
+        var httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+        return DockerClientImpl.getInstance(config, httpClient);
+    }
+}
